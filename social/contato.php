4c00d4ec46b1d6dc57a5dbddaea9264f7f2ef0e9
Marcelo Gaia <marcelozakk07@gmail.com>
2016-07-07 16:59:52 -0700

    Trying to solve iPhone scrolling problem

--- social/contato.php
+++ social/contato.php
@@ -68,7 +68,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no, minimum-scale=1, maximum-scale=1">
-<meta name="apple-mobile-web-app-capable" content="yes" />
+
 	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 	<meta name="description" content="">
 	<meta name="author" content="">
